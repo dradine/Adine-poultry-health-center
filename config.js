45aa2f652e975c5769ade5b931654be3e99bd2b6
dradine <dradine.vet@gmail.com>
2026-08-18 15:01:17 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
 
+const SUPABASE_URL = "vzcczkavlopznljnnehp";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_4jMgvqKI__-MsmMQtEiCig_M9WjhvN9";
+
+window.supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
